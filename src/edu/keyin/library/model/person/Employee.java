e6d3eef933b9f4298ab96a93d29672b32c40514a
BradTheeStallion <bradley.ayers@keyin.com>
2024-10-22 20:45:09 -0230

    1st draft of Patron and extensions (Employee and Student)

--- src/edu/keyin/library/model/person/Employee.java
+++ src/edu/keyin/library/model/person/Employee.java
@@ -1,4 +1,27 @@
 package edu.keyin.library.model.person;
 
-public class Employee {
+public class Employee extends Patron {
+    private String employeeId;
+
+    public Employee(String name, String address, String phoneNumber, String employeeId) {
+        super(name, address, phoneNumber);
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", borrowed items=" + getBorrowedItems().size() +
+                '}';
+    }
 }
